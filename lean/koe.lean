5792c0eae0ee6c1967310846c6a6374bfa0269c8
Aarne Ranta <aarne@chalmers.se>
2024-02-09 08:18:12 +0100

    trying to get do and for to work with layout

--- lean/koe.lean
+++ lean/koe.lean
@@ -9,7 +9,7 @@ def four' := 2 + 2
 def isOne' x := if x = 1 then "yes" else "no"
 
 def Fermat_statement : Prop :=
-∀ a b c n : Nat, a * b * c ≠ 0 ∧ n > 2 → a^n + b^n ≠ c^n
+  ∀ a b c n : Nat, a * b * c ≠ 0 ∧ n > 2 → a^n + b^n ≠ c^n
 
 theorem two_plus_two_is_four : 2 + 2 = 4 := rfl
 
@@ -21,8 +21,8 @@ def bar n := foo (foo n) + 3
 
 /- parsed 240207 although not properly -/
 def printExample : IO Unit:= do
-  IO.println "hello" ;
-  IO.println "world" ;
+  IO.println "hello"
+  IO.println "world" ; /- semicolon ad hoc -/
 
 def factorial : Nat → Nat
   | 0       => 1
@@ -30,11 +30,12 @@ def factorial : Nat → Nat
 
 def hanoi (numDisks start finish aux : Nat) : IO Unit :=
   match numDisks with
-  | 0     => pure ()
-  | n + 1 => do
-      hanoi n start aux finish ;
-      IO.println s!"Move disk {n + 1} from peg {start} to peg {finish}" ;
-      hanoi n aux finish start ;
+  | 0     => pure () 
+  | n + 1 => /- do
+      hanoi n start aux finish
+      IO.println s!"Move disk {n + 1} from peg {start} to peg {finish}" -/
+      hanoi n aux finish start 
+
 
 def addNums : List Nat → Nat
   | []    => 0
@@ -107,10 +108,11 @@ def bar (n? : Option Nat) : Nat :=
   | some n => n
   | none   => 0
 
-def showSums : IO Unit := do
-  let mut sum := 0 ;
+
+def showSums : IO Unit := /- do
+  let mut sum := 0 
   for i in [0:100] do
-    sum := sum + i ;
-    IO.println s!"i: {i}, sum: {sum}" ;
+    sum := sum + i -/
+    IO.println s!"i: {i}, sum: {sum}" 
 
 
