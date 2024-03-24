6e3a76d46ddd396452d23a15f0d692f485f3c8de
Aarne Ranta <aarne@chalmers.se>
2024-03-24 10:17:05 +0100

    collected missing words from Chicago

--- data/get_chicago.py
+++ data/get_chicago.py
@@ -10,7 +10,8 @@ contents = {}
 
 
 def text_only(s: str) -> str:
-    r = re.sub("([\$]).*?([\$])", "\g<1>\g<2>", s)  # delete inside $ $
+    r = s.replace('$$', '$')                        # eliminate double $$
+    r = re.sub("([\$]).*?([\$])", "\g<1>\g<2>", r)  # delete inside $ $
     r = re.sub("([\(]).*?([\)])", "\g<1>\g<2>", r)  # delete inside ( )
     r = r.replace('$$', 'SYMBOL')
     r = ''.join([c for c in r if c not in '()[]*'])
@@ -32,7 +33,7 @@ for f in chicago_files:
         b = file.read()
         text = b.split('\n')
         if len(text) > 7:
-            contents[f] = (text_only(text[5]), get_qid(text[7]), text[7], 2)
+            contents[f] = (text_only('\n'.join(text[5:-2])), get_qid(text[-1]), text[-1], 2)
         elif len(text) > 5:
             contents[f] = (text_only(text[5]), 1)
         else:
@@ -40,7 +41,8 @@ for f in chicago_files:
 
 
 for f in contents:
-    print(f, contents[f])
+#    print(f, contents[f])
+    print(contents[f][0])
 
 print(len(chicago_files), len(contents))
 
