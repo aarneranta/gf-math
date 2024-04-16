693fb3a9cab326f3d700c5ab7f48927dabf4110c
Aarne Ranta <aarne@chalmers.se>
2024-04-16 12:00:33 +0200

    grammar for lean-lang 1

--- lean/fp.lean
+++ lean/fp.lean
@@ -140,3 +140,246 @@ def plus (n : Nat) (k : Nat) : Nat :=
   | Nat.succ k' => Nat.succ (plus n k')
 
 
+-- 1.6
+
+structure PPPoint (α : Type) where
+  x : α
+  y : α
+ deriving Repr
+
+def natOrigin : PPPoint Nat :=
+  { x := Nat.zero, y := Nat.zero }
+
+def replaceX (α : Type) (point : PPPoint α) (newX : α) : PPPoint α :=
+  { point with x := newX }
+
+#check (replaceX)
+#check replaceX Nat
+
+#eval replaceX Nat natOrigin 5
+
+inductive Sign where
+  | pos
+  | neg
+
+def posOrNegThree (s : Sign) : match s with | Sign.pos => Nat | Sign.neg => Int :=
+  match s with
+  | Sign.pos => (3 : Nat)
+  | Sign.neg => (-3 : Int)
+
+
+-- 1.6
+
+def primesUnder10 : List Nat := [2, 3, 5, 7]
+
+inductive LList (α : Type) where
+  | nil : LList α
+  | cons : α → LList α → LList α
+
+def explicitPrimesUnder10 : List Nat :=
+  List.cons 2 (List.cons 3 (List.cons 5 (List.cons 7 List.nil)))
+
+def length (α : Type) (xs : List α) : Nat :=
+  match xs with
+  | List.nil => Nat.zero
+  | List.cons y ys => Nat.succ (length α ys)
+
+
+def llength (α : Type) (xs : List α) : Nat :=
+  match xs with
+  | [] => 0
+  | y :: ys => Nat.succ (llength α ys)
+
+def rreplaceX {α : Type} (point : PPoint α) (newX : α) : PPoint α :=
+  { point with x := newX } --- error: 'sorryAx' is not a structure
+
+#eval rreplaceX natOrigin 5
+
+def lllength {α : Type} (xs : List α) : Nat :=
+  match xs with
+  | [] => 0
+  | y :: ys => Nat.succ (lllength ys)
+
+#eval lllength primesUnder10
+
+#check List.length (α := Int)
+
+inductive OOption (α : Type) : Type where
+  | none : OOption α
+  | some (val : α) : OOption α
+
+
+def List.hhead? {α : Type} (xs : List α) : Option α :=
+  match xs with
+  | [] => none
+  | y :: _ => some y
+
+#eval primesUnder10.hhead?
+#eval [].hhead? (α := Int)
+#eval ([] : List Int).hhead?
+
+structure PPProd (α : Type) (β : Type) : Type where
+  fst : α
+  snd : β
+
+def fives : String × Int := { fst := "five", snd := 5 }
+def ffives : String × Int := ("five", 5)
+
+inductive SSum (α : Type) (β : Type) : Type where
+  | inl : α → SSum α β
+  | inr : β → SSum α β
+
+def PetName : Type := String ⊕ String
+
+def animals : List PetName :=
+  [Sum.inl "Spot", Sum.inr "Tiger", Sum.inl "Fifi", Sum.inl "Rex", Sum.inr "Floof"]
+
+def howManyDogs (pets : List PetName) : Nat :=
+  match pets with
+  | [] => 0
+  | Sum.inl _ :: morePets => howManyDogs morePets + 1
+  | Sum.inr _ :: morePets => howManyDogs morePets
+
+inductive UUnit : Type where
+  | unit : UUnit
+
+inductive ArithExpr (ann : Type) : Type where
+  | int : ann → Int → ArithExpr ann
+  | plus : ann → ArithExpr ann → ArithExpr ann → ArithExpr ann
+  | minus : ann → ArithExpr ann → ArithExpr ann → ArithExpr ann
+  | times : ann → ArithExpr ann → ArithExpr ann → ArithExpr ann
+
+#eval (() : Unit)
+
+
+-- 1.7
+
+def lllength {α : Type} (xs : List α) : Nat :=
+  match xs with
+  | [] => 0
+  | y :: ys => Nat.succ (lllength ys)
+
+def llllength (xs : List α) : Nat :=
+  match xs with
+  | [] => 0
+  | y :: ys => Nat.succ (llllength ys)
+
+def lllllength : List α → Nat
+  | [] => 0
+  | y :: ys => Nat.succ (lllllength ys)
+
+def drop : Nat → List α → List α
+  | Nat.zero, xs => xs
+  | _, [] => []
+  | Nat.succ n, x :: xs => drop n xs
+
+
+def fromOption (default : α) : Option α → α
+  | none => default
+  | some x => x
+
+#eval (some "salmonberry").getD ""
+
+#eval none.getD ""
+
+def unzip : List (α × β) → List α × List β
+  | [] => ([], [])
+  | (x, y) :: xys =>
+    (x :: (unzip xys).fst, y :: (unzip xys).snd)
+
+def uunzip : List (α × β) → List α × List β
+  | [] => ([], [])
+  | (x, y) :: xys =>
+    let unzipped : List α × List β := uunzip xys
+    (x :: unzipped.fst, y :: unzipped.snd)
+-- To use let on a single line, separate the local definition from the body with a semicolon.
+
+def uuunzip : List (α × β) → List α × List β
+  | [] => ([], [])
+  | (x, y) :: xys =>
+    let (xs, ys) : List α × List β := uuunzip xys
+    (x :: xs, y :: ys)
+
+def reverse (xs : List α) : List α :=
+  let rec helper : List α → List α → List α
+    | [], soFar => soFar
+    | y :: ys, soFar => helper ys (y :: soFar)
+  helper xs []
+
+def uuuunzip (pairs : List (α × β)) :=
+  match pairs with
+  | [] => ([], [])
+  | (x, y) :: xys =>
+    let unzipped := uuuunzip xys
+    (x :: unzipped.fst, y :: unzipped.snd)
+
+def ddrop (n : Nat) (xs : List α) : List α :=
+  match n, xs with
+  | Nat.zero, ys => ys
+  | _, [] => []
+  | Nat.succ n , y :: ys => ddrop n ys
+
+def eeven : Nat → Bool
+  | 0 => true
+  | n + 1 => not (eeven n)
+
+def halve : Nat → Nat
+  | 0 => 0
+  | 1 => 0
+  | n + 2 => halve n + 1
+
+#check fun x => x + 1
+
+#check fun {α : Type} (x : α) => x
+#check λ {α : Type} (x : α) => x
+
+#check fun
+  | 0 => none
+  | n + 1 => some n
+
+def double : Nat → Nat := fun
+  | 0 => 0
+  | k + 1 => double k + 2
+
+#eval (· * 2) 5
+
+def Nat.double (x : Nat) : Nat := x + x
+
+#eval (4 : Nat).double
+
+namespace NewNamespace
+def triple (x : Nat) : Nat := 3 * x
+def quadruple (x : Nat) : Nat := 2 * x + 2 * x
+end NewNamespace
+
+#check NewNamespace.triple
+#check NewNamespace.quadruple
+
+def timesTwelve (x : Nat) :=
+  open NewNamespace in
+  quadruple (triple x)
+
+open NewNamespace in
+  #check quadruple
+
+inductive Inline : Type where
+  | lineBreak
+  | string : String → Inline
+  | emph : Inline → Inline
+  | strong : Inline → Inline
+
+def Inline.string? (inline : Inline) : Option String :=
+  match inline with
+  | Inline.string s => some s
+  | _ => none
+
+def Inline.sstring? (inline : Inline) : Option String :=
+  if let Inline.string s := inline then
+    some s
+  else none
+
+#eval (⟨1, 2⟩ : Point)
+
+#eval s!"three fives is {NewNamespace.triple 5}"
+
+  
