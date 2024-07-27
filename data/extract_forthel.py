299b738e9a2de610c1014a77ad1cc90a414b7f69
Aarne Ranta <aarne@chalmers.se>
2024-07-27 20:45:13 +0200

    analysing an article

--- data/extract_forthel.py
+++ data/extract_forthel.py
@@ -1,20 +1,63 @@
 import sys
 
-forthel_lines = []
+forthel_segments = []
+segment = []
 
 in_forthel = False
 
 for line in sys.stdin:
-    if line.strip() == '\\begin{forthel}':
+    line = line.strip()
+    if line == '\\begin{forthel}':
         in_forthel = True
-    elif line.strip() == '\\end{forthel}':
+    elif line == '\\end{forthel}':
         in_forthel = False
-    elif line.startswith('%'):
+        forthel_segments.append(' '.join(segment))
+        segment = []
+    elif line.startswith('%'):   ## to be kept
+        pass
+    elif line.startswith('[timelimit'):  ## to be kept
+        pass
+    elif line.startswith('\\begin{'):   ## to be kept
+        pass
+    elif line.startswith('\\end{'):  ## to be kept
         pass
     elif in_forthel:
-        forthel_lines.append(line)
+        segment.append(line)
     else:
         pass
 
+def indexed(i):
+    return '$\\INDEXEDTERM{' + str(i) + '}$' 
+    
+math_dict = {}
+counter = 1
+analysed_segments = []
+
+for segment in forthel_segments:
+    asegment = []
+    segment = segment.strip()
+    math_in = 0 if segment and segment[0] == '$' else 1
+    parts = [s for part in segment.split('$$')
+               for s in part.split('$')]
+    for i in range(len(parts)):
+        if math_in and i % 2 == 1:
+            asegment.append(indexed(counter))
+            math_dict[counter] = parts[i]
+            counter += 1
+        else:
+            asegment.append(parts[i])
+    asegment = ' '.join(asegment)
+    asegment = asegment.split('.')
+    asegment = [s + '.' for s in asegment]
+    analysed_segments.extend(asegment)
+
+
+for a in analysed_segments:
+    print(a)
+
+for k in math_dict:
+    pass
+#    print(k, math_dict[k])
+    
 
 
