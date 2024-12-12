404a2c3618ef42cec28bec222e713fe2fceead6b
Aarne Ranta <aarne@chalmers.se>
2024-12-12 11:36:01 +0100

    analysing MMA dataset

---
+++ data/mma/analyse_mma.py
@@ -0,0 +1,23 @@
+import json
+
+ISABELLE_TRAIN = 'mma-dataset/isabelle_train.jsonl'
+ISABELLE_VAL = 'mma-dataset/isabelle_val.jsonl'
+LEAN_TEST = 'mma-dataset/lean_test.jsonl'
+LEAN_TEST = 'mma-dataset/lean_test.jsonl'
+LEAN_TRAIN = 'mma-dataset/lean_train.jsonl'
+LEAN_VAL = 'mma-dataset/lean_val.jsonl'
+
+
+def get_data(filename):
+    with open(filename) as file:
+        data = [json.loads(line) for line in file]
+    return data
+
+
+for ex in get_data(LEAN_TEST):
+#    print('#ENG', ex['input'])
+#    print('#LEAN', ex['output'])
+    print(ex['output'])
+
+
+
