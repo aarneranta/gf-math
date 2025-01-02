59b275865086c3404b5f91546e6029a1a396fc57
Aarne Ranta <aarne@chalmers.se>
2025-01-02 19:02:14 +0100

    analysing mma further

--- data/mma/analyse_mma.py
+++ data/mma/analyse_mma.py
@@ -1,3 +1,4 @@
+import sys
 import json
 
 ISABELLE_TRAIN = 'mma-dataset/isabelle_train.jsonl'
@@ -7,6 +8,16 @@ LEAN_TEST = 'mma-dataset/lean_test.jsonl'
 LEAN_TRAIN = 'mma-dataset/lean_train.jsonl'
 LEAN_VAL = 'mma-dataset/lean_val.jsonl'
 
+MODE = 'split'
+if sys.argv[1:]:
+    MODE = sys.argv[1]
+
+DATASET = LEAN_TRAIN
+if sys.argv[2:]:
+    DATASET = sys.argv[2]
+
+TOPMOST = 1000
+    
 
 def get_data(filename):
     with open(filename) as file:
@@ -14,10 +25,55 @@ def get_data(filename):
     return data
 
 
-for ex in get_data(LEAN_TEST):
-#    print('#ENG', ex['input'])
-#    print('#LEAN', ex['output'])
-    print(ex['output'].replace('\n', '  '), 'sorry')
+def tokenize(s):
+    cs = []
+    for c in s:
+        if c in '.,:()[]{}':
+            cs.extend([' ', c, ' '])
+        else:
+            cs.append(c)
+    return [w.lower() for w in ''.join(cs).split()]
+
+
+def print_token_statistics(tokens):
+        toks = {}
+        for t in tokens:
+            toks[t] = toks.get(t, 0) + 1
+        tok_freqs = sorted(list(toks.items()), key=lambda t: -t[1])
+        for t, n in tok_freqs[:TOPMOST]:
+            print(t, n)
+        print(len(toks))
+
+
+def wrap_input(s):
+    input_wrap = ('Statement in natural language: ',
+              ' Translate the statement in natural language to Lean:')
+    pref, suff = [len(x) for x in input_wrap]
+    return s[pref:-suff].replace('\n', '  ')
+
+
+def wrap_output_lean(s):
+    return s.replace('\n', '  ') + ' sorry'
+
+
+def process(dataset, mode):
+    data = get_data(dataset)
+    if mode == 'words':
+        print_token_statistics(t for ex in data for t in tokenize(ex['input']))
+
+    elif mode == 'symbols':
+        print_token_statistics(t for ex in data for t in tokenize(ex['output']))
+        
+    else:
+        for ex in data:
+            print(wrap_input(ex['input']))
+            print(wrap_output_lean(ex['output']))
+
+
+
+    
+if __name__ == '__main__':
+    process(DATASET, MODE)
 
 
 
