5bed706931749484193f9b447ef93e83f5bc2745
aarneranta <aarne@chalmers.se>
2025-01-30 16:30:09 +0100

    minimal Lean grammar to parse and generate the relevant fragment

--- v3/BaseConstants.lean
+++ v3/BaseConstants.lean
@@ -1,4 +1,4 @@
-def false : Prop := False
+-- def false : Prop := False
 -- def And (A : Prop)(B : Prop) : Prop := And A B
 -- def Or (A : Prop)(B : Prop) : Prop := Or A B
 def If (A B : Prop) : Prop := A -> B
@@ -22,9 +22,9 @@ def Complex : Type := Num
 --def Eq (x : Real) (y : Real) : Prop := Eq x y
 def Lt (x : Int) (y : Int) : Prop := x < y
 def Gt (x : Int) (y : Int) : Prop := x > y
-def Neq (x : Int) (y : Int) : Prop := Not (x = y)
-def Leq (x : Int) (y : Int) : Prop := Not (x > y)
-def Geq (x : Int) (y : Int) : Prop := Not (x < y)
+def Neq (x : Int) (y : Int) : Prop := Not (Eq x y)
+def Leq (x : Int) (y : Int) : Prop := Not (Gt x y)
+def Geq (x : Int) (y : Int) : Prop := Not (Lt x y)
 
 def positive (x : Int) : Prop := x > 0
 def negative (x : Int) : Prop := x < 0
@@ -34,15 +34,16 @@ def minus (x y : Int) : Int := x - y
 def times (x y : Int) : Int := x * y
 def div (x y : Int) : Int := x / y
 ---def pow (x y : Int) : Int := x ^ y  --- failed to synthesize instance HPow Int Int ?m.392
-def gcd (x y : Int) : Int := sorry ---
-def factorial (x : Int) : Int := sorry ---
+axiom gcd (x y : Int) : Int
+axiom factorial (x : Int) : Int
 
-def even (n : Int) : Prop := exists m : Int, n = 2*m
+def even (n : Int) : Prop := Exist Int (fun m => Eq n (times 2 m))
 def odd (n : Int) : Prop := Not (even n)
 
-def divisible (n m : Int) : Prop := exists x : Int, n = x * m
+-- def divisible (n m : Int) : Prop := exists x : Int, n = x * m
+def divisible (n m : Int) : Prop := Exist Int (fun k => Eq n (times k m))
 
-def prime (n : Nat) : Prop := Not (exists m : Nat, And (1 < m) (And (m < n) (divisible n  m)))
+def prime (n : Nat) : Prop := Not (Exist Nat (fun m => And (Lt 1 m) (And (Lt m n) (divisible n  m))))
 
 /-
 (; set operations ;)
