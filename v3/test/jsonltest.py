340c9aa5a59c61fb41efb8f8ea90daa1c7c25f67
aarneranta <aarne@chalmers.se>
2025-02-26 13:38:53 +0100

    a script and readme instructions for building synthetic data

--- v3/test/jsonltest.py
+++ v3/test/jsonltest.py
@@ -1,10 +1,26 @@
+import sys
 import json
 
-with open('exx.jsonl') as file:
+if sys.argv[1:]:
+    JSONL_FILE = sys.argv[1]
+else:
+    print('usage: jsonltest.py <file> <formalkey>? <informalkey>?')
+    exit()
+
+with open(JSONL_FILE) as file:
     data = [json.loads(line) for line in file]
 
-print(len(data), 'formulas')
-print(sum([len(v) for d in data for k, v in d.items() if k == 'InformathEng']), 'sentences')
+if sys.argv[3:]:
+    formal = sys.argv[2]
+    informal = sys.argv[3]
+    corpus = [{formal: d[formal], informal: e} for d in data for e in d[informal]]
+    for item in corpus:
+        print(json.dumps(item, ensure_ascii=False))
+
+    
+else:
+   print(len(data), 'formulas')
+   print(sum([len(v) for d in data for k, v in d.items() if k == 'InformathEng']), 'sentences')
 
 
          
